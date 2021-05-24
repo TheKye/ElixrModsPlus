18212b3c641676e00f1d15eece3cb8c6424edabe
Fyre <ross.ashley.t@gmail.com>
2021-05-24 23:14:07 +1000

    ..

---
+++ ElixrModsPlusProject/Conv Pack/EM-GreenEnergy-Plus/Recipes/DaylightSensorOveride.cs
@@ -0,0 +1,27 @@
+﻿using Eco.EM.GreenEnergy;
+using Eco.EM.ModTools.RecipeResolver;
+using Eco.Gameplay.Items;
+using Eco.Mods.TechTree;
+using Eco.Shared.Localization;
+using System.Collections.Generic;
+
+namespace Eco.EM.Overrides
+{
+    public class DaylightSensorWallOverrideRecipe: IRecipeOverride
+    {
+        public static Type OverrideType => typeof(DaylightSensorWallRecipe);
+
+        public static List<Recipe> NewRecipe => new List<Recipe>() 
+        {
+            new Recipe(
+            "TheBob",
+            Localizer.DoStr("The Bob"),
+            new IngredientElement[]
+            {
+                new IngredientElement("WoodBoard", 10),
+            },
+            new CraftingElement<DaylightSensorWallItem>(1)
+            )
+        };        
+    }
+}
