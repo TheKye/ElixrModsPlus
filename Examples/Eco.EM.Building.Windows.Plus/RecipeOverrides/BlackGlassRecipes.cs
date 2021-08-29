aaa41553cb51d0aac44e3c04dcafac1430cd4580
kye <kye-a@hotmail.com>
2021-08-29 21:18:50 +1000

    Updated for the New System, Added Example for a recipe Override,

---
+++ Examples/Eco.EM.Building.Windows.Plus/RecipeOverrides/BlackGlassRecipes.cs
@@ -0,0 +1,32 @@
+﻿using Eco.EM.Framework;
+using Eco.EM.Framework.DataStructures;
+using Eco.EM.Artistry;
+using Eco.EM.Building.Windows;
+
+namespace Eco.EM.Building.Windows.Plus
+{
+    public partial class BlackGlassOverrideRecipe : IRecipeOverride
+    {
+        public string OverrideType => typeof(BlackGlassRecipe).Name;
+        public RecipeModel Model => new()
+        {
+            ModelType = typeof(BlackGlassRecipe).Name,
+            Assembly = typeof(BlackGlassRecipe).AssemblyQualifiedName,
+            IngredientList = new()
+            {
+                new EMIngredient("GlassItem", false, 6, true),
+                new EMIngredient("BlackDyeItem", false, 1, true)
+            },
+            ProductList = new()
+            {
+                new EMCraftable("GlassBlackItem", 6),
+            },
+            BaseExperienceOnCraft = 1,
+            BaseLabor = 50,
+            LaborIsStatic = false,
+            BaseCraftTime = 2,
+            CraftTimeIsStatic = false,
+            CraftingStation = "GlassworkingTableItem",
+        };
+    }
+}
