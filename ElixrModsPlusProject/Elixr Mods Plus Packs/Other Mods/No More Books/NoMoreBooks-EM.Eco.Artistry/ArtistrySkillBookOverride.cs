c9fa64261823903adbda19927de58d4942b47d87
kye <kye-a@hotmail.com>
2021-10-15 13:41:32 +1100

    Added Plus Modules for NoMoreBooks

---
+++ ElixrModsPlusProject/Elixr Mods Plus Packs/Other Mods/No More Books/NoMoreBooks-EM.Eco.Artistry/ArtistrySkillBookOverride.cs
@@ -0,0 +1,37 @@
+﻿using Eco.EM.Artistry;
+using Eco.EM.Framework.Resolvers;
+using System;
+
+namespace NoMoreBooks_EM.Eco.Artistry
+{
+    public partial class DefaultPaintingBookOverride : IRecipeOverride
+    {
+        public string OverrideType => typeof(PaintingSkillBookRecipe).Name;
+        public RecipeModel Model => new()
+        {
+            //Required for internal referencing
+            ModelType = typeof(PaintingSkillBook).Name,
+            Assembly = typeof(PaintingSkillBook).AssemblyQualifiedName,
+
+            // List of new ingredients using the EM Ingredient
+            IngredientList = new()
+            {
+                new EMIngredient("PaperItem", false, 40),
+                new EMIngredient("ClothItem", false, 10),
+                new EMIngredient("WoodBoard", true, 20),
+                new EMIngredient("Basic Research", true, 5)
+            },
+
+            // List of new Products to output
+            ProductList = new()
+            {
+                new EMCraftable("PaintingSkillScroll", 1),
+            },
+
+            //Recipe is a Variant of a Parent Recipe, Only Crafting Table is needed
+            CraftingStation = "ResearchTableItem",
+        };
+
+        public bool debug => true;
+    }
+}
---
+++ ElixrModsPlusProject/Elixr Mods Plus Packs/Other Mods/No More Books/NoMoreBooks-Eco.EM.Zymology/ZymologySkillBookOverride.cs
@@ -0,0 +1,43 @@
+﻿using Eco.EM.Framework.Resolvers;
+using System;
+using Eco.EM.Food.Zymology;
+
+namespace NoMoreBooks_Eco.EM.Zymology
+{
+    public partial class DefaultZymologyBookOverride : IRecipeOverride
+    {
+        public string OverrideType => typeof(ZymologySkillBookRecipe).Name;
+        public RecipeModel Model => new()
+        {
+            //Required for internal referencing
+            ModelType = typeof(ZymologySkillBook).Name,
+            Assembly = typeof(ZymologySkillBook).AssemblyQualifiedName,
+
+            // List of new ingredients using the EM Ingredient
+            IngredientList = new()
+            {
+                new EMIngredient("Lumber", true, 40),
+                new EMIngredient("MortarItem", false, 50),
+                new EMIngredient("ClothItem", false, 20),
+                new EMIngredient("VegetableSoupItem", false, 10),
+                new EMIngredient("CulinaryResearchPaperBasicItem", false, 5)
+            },
+
+            // List of new Products to output
+            ProductList = new()
+            {
+                new EMCraftable("ZymologySkillScroll", 1),
+            },
+
+            //Recipe is a Variant of a Parent Recipe, Only Crafting Table is needed
+            BaseExperienceOnCraft = 1,
+            BaseLabor = 2000,
+            LaborIsStatic = true,
+            BaseCraftTime = 15,
+            CraftTimeIsStatic = true,
+            CraftingStation = "ResearchTableItem",
+        };
+
+        public bool debug => false;
+    }
+}
